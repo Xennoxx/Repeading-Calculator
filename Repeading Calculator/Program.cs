20257384848b50565379243dab905f0127dfd3e8
Xennoxx <nicknifffe@gmail.com>
2023-02-17 07:00:51 +0100

    Change 1

--- Repeading Calculator/Program.cs
+++ Repeading Calculator/Program.cs
@@ -1,10 +1,31 @@
-﻿class program
+﻿class Program
 {
-    public void main(string[] args)
+    static void Main(string[] args)
     {
+        //[A1] Objekt Aufgabe erstellt
         wert Aufgabe1 = new wert();
-        Aufgabe1.wert1 = Console.ReadLine();
-        Aufgabe1.zeicheneingabe = Console.ReadLine();
+
+        //[A1] Eingabe von Wert1
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.Write("Wert1: ");
+        Aufgabe1.wert1 = Convert.ToInt32(Console.ReadLine());
+        Console.Clear();
+
+        //[A1] Eingabe von Wert2
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("Wert2: ");
+        Aufgabe1.wert2 = Convert.ToInt32(Console.ReadLine());
+        Console.Clear();
+
+        //[A1] Eingabe von "Zeicheneingabe"
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Zeicheneingabe: ");
+        Aufgabe1.zeicheneingabe = Convert.ToChar(Console.ReadLine());
+        Console.Clear();
+
+        //[A1] Ausgabe von, Wert1,Wert2,"Zeicheneingabe"
+        Console.WriteLine("Werte:" + Aufgabe1.wert1 + Aufgabe1.wert2 + Aufgabe1.zeicheneingabe);
+        Console.ReadKey();
     }
 }
 
