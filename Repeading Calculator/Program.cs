550ddce952896416e7c74c996e8d97005be54d23
Xennoxx <nicknifffe@gmail.com>
2023-02-17 06:37:10 +0100

    Projektdateien hinzufügen.

---
+++ Repeading Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿class program
+{
+    public void main(string[] args)
+    {
+        wert Aufgabe1 = new wert();
+        Aufgabe1.wert1 = Console.ReadLine();
+        Aufgabe1.zeicheneingabe = Console.ReadLine();
+    }
+}
+
+class wert
+{
+    public int wert1 { get; set; }
+    public int wert2 { get; set; }
+    public char zeicheneingabe { get; set; }
+}
